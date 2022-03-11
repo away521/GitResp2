ea604624d433ae810f6ba73b689fde2560643f05
away521 <275298909@qq.com>
2022-03-12 04:52:52 +0800

    添加学生类

---
+++ testgit/src/student.java
@@ -0,0 +1,10 @@
+/**
+ * @Authere: jiangwei
+ * @Date: 2022/3/12 -03 -12 -4:51
+ * @Description: PACKAGE_NAME
+ * @vertion: 1.0
+ */
+public class student {
+    private String name;
+    private String sex;
+}
