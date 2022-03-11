e418de89dfbf5e2ee10b3329a071397e5304710d
away521 <275298909@qq.com>
2022-03-12 04:59:30 +0800

    添加教师类

---
+++ testgit/src/teacher.java
@@ -0,0 +1,10 @@
+/**
+ * @Authere: jiangwei
+ * @Date: 2022/3/12 -03 -12 -4:58
+ * @Description: PACKAGE_NAME
+ * @vertion: 1.0
+ */
+public class teacher {
+    private String tname;
+    private String tage;
+}
